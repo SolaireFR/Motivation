a2e520fa21bbbcfaacb64c801a3570b67d38c6bf
m.secretin <m.secretin@iesi.info>
2025-05-30 10:24:34 +0200

    App and Tests : Clear app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<router-outlet />`,
 })
-export class App {
-  protected title = 'motivation-frontend';
-}
+export class App {}
